aed17752baec2f3598c3783683cba034f957f0a8
dekel241 <dekel241@gmail.com>
2020-05-05 01:30:13 +0300

    Redesign code -Add abstruct class(pure virtual) -Adjust Real and Complex to inherit from it -Add new variables a,b,c

--- solver.hpp
+++ solver.hpp
@@ -13,11 +13,31 @@ namespace solver{
     double solve(RealVariable);//Function has to clear the varables of the reference
     complex<double> solve(ComplexVariable);
 
-    class RealVariable{
-        double _re;
+    class Variable{
+    protected:
+        double _a;//Coefficient of x^2
+        double _b;//Coefficient of x
+        double _c;//Free number
+
+        Variable(): _a(0.0), _b(1.0), _c(0.0){}//Default C'tor
+        Variable(double a, double b, double c): _a(a), _b(b), _c(c){}
+        virtual ~Variable() = 0;//Yes i make it pure virtual => abstract class
+    public:
+        const double a() const {return _a;}
+        const double b() const {return _b;}
+        const double c() const {return _c;}
+    };
+
+    class RealVariable: public Variable{
+        double solve_abc_formula(RealVariable & r);
 
     public:
-        RealVariable(): _re(1.0){};//Default C'tor
+        // Inherit Base's constructors, default ctor is Inherited by default(if not other Ctor implemented)
+        using Variable::Variable;
+
+        // Equivalent to:
+        //RealVariable(double a, double b, double c): Variable(a,b,c){};
+
         //We want make it friend in order to enable something like 7 * RealVariable
         friend RealVariable operator +(const RealVariable &, const RealVariable &);
         friend RealVariable operator +(const RealVariable &, const int);
@@ -31,13 +51,12 @@ namespace solver{
         friend RealVariable operator ==(const RealVariable &, const int);
     };
 
-    class ComplexVariable {
+    class ComplexVariable: public Variable {
         double _im;
-        double _re;
+        ComplexVariable(double a, double b, double c, double im): Variable(a,b,c), _im(im){}
 
     public:
-        ComplexVariable(): _im(0.0), _re(0.0){}; //Default C'tor
-
+        ComplexVariable(): Variable(), _im(0.0){};
         friend ComplexVariable operator +(const ComplexVariable &, const ComplexVariable &);
         friend ComplexVariable operator +(const ComplexVariable &, const int);
         friend ComplexVariable operator +(const int, const ComplexVariable &);
