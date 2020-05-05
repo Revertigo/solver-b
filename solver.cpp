1c0ca8b3e6d6d8643d70e52c0d6c2e907414285b
dekel241 <dekel241@gmail.com>
2020-05-06 00:29:43 +0300

    Code refactor -Static code formatter

--- solver.cpp
+++ solver.cpp
@@ -2,74 +2,72 @@
 #include <iostream>
 #include <exception>
 
-namespace solver{
+namespace solver {
     //RealVariable class
-    RealVariable operator +(const RealVariable & r1, const RealVariable & r2){
+    RealVariable operator+(const RealVariable &r1, const RealVariable &r2) {
         return RealVariable(r1._a + r2._a, r1._b + r2._b, r1._c + r2._c);
     }
 
-    RealVariable operator +(const RealVariable & r, const double num){
+    RealVariable operator+(const RealVariable &r, const double num) {
         return RealVariable(r._a, r._b, r._c + num);
     }
 
-    RealVariable operator +(const double num, const RealVariable & r){
+    RealVariable operator+(const double num, const RealVariable &r) {
         return r + num; // :)
     }
 
-    RealVariable operator -(const RealVariable & r1, const RealVariable & r2){
+    RealVariable operator-(const RealVariable &r1, const RealVariable &r2) {
         return RealVariable(r1._a - r2._a, r1._b - r2._b, r1._c - r2._c);
     }
 
-    RealVariable operator -(const RealVariable & r, const double num){
+    RealVariable operator-(const RealVariable &r, const double num) {
         return RealVariable(r._a, r._b, r._c - num);
     }
 
-    RealVariable operator *(const double num, const RealVariable & r){
+    RealVariable operator*(const double num, const RealVariable &r) {
         return r._a > 0 ? RealVariable(r._a * num, r._b, r._c) : RealVariable(r._a, num * r._b, r._c);
     }
 
-    RealVariable operator /(const RealVariable & r , const double num){
-        if(num == 0){
+    RealVariable operator/(const RealVariable &r, const double num) {
+        if (num == 0) {
             throw invalid_argument("Can't divide by zero");
         }
 
-        return r._a > 0 ? RealVariable(r._a/num, r._b, r._c) : RealVariable(r._a, r._b / num, r._c);
+        return r._a > 0 ? RealVariable(r._a / num, r._b, r._c) : RealVariable(r._a, r._b / num, r._c);
     }
 
-    RealVariable operator ^(RealVariable const & r, const double num){
-        if(num == 0.0){
+    RealVariable operator^(RealVariable const &r, const double num) {
+        if (num == 0.0) {
             throw invalid_argument("can't power by zero");
         }
         return RealVariable(1, 0, r._c); //Sign it as x^2(we don't want 1x also)
     }
 
-    RealVariable operator ==(const RealVariable & r1, const RealVariable & r2){
+    RealVariable operator==(const RealVariable &r1, const RealVariable &r2) {
         return r1 - r2;
     }
 
-    RealVariable operator ==(const RealVariable & r, const double num){
+    RealVariable operator==(const RealVariable &r, const double num) {
         return r - num;
     }
 
-    complex<double> RealVariable::solve_abc_formula(void)
-    {
+    complex<double> RealVariable::solve_abc_formula(void) {
         double a(_a), b(_b), c(_c);
-        double discriminant = b*b - 4*a*c;
+        double discriminant = b * b - 4 * a * c;
         complex<double> result = 0.0;
 
-        if(a == 0.0 && b == 0.0)
-        {
+        if (a == 0.0 && b == 0.0) {
             throw invalid_argument("No real soultion exists");
         }
-        if(a != 0)//It is possible to be a Linear equation
+        if (a != 0)//It is possible to be a Linear equation
         {
             if (discriminant >= 0) {
                 result = (-b + sqrt(discriminant)) / (2 * a);
             } else {
                 throw exception(invalid_argument("There is no solution"));
             }
-        } else{
-            result = -c/b; //The only solution we have
+        } else {
+            result = -c / b; //The only solution we have
         }
 
         return result;
@@ -80,67 +78,68 @@ namespace solver{
     }
 
     //ComplexVariable class
-    ComplexVariable operator +(const ComplexVariable & c1, const ComplexVariable & c2){
+    ComplexVariable operator+(const ComplexVariable &c1, const ComplexVariable &c2) {
         return ComplexVariable(c1._a + c2._a, c1._b + c2._b, c1._c + c2._c, c1._im + c2._im);
     }
 
-    ComplexVariable operator +(const ComplexVariable & c, const double num ){
+    ComplexVariable operator+(const ComplexVariable &c, const double num) {
         return ComplexVariable(c._a, c._b, c._c + num, c._im);
     }
 
-    ComplexVariable operator +(const double num, const ComplexVariable & c){
+    ComplexVariable operator+(const double num, const ComplexVariable &c) {
         return c + num;
     }
 
-    ComplexVariable operator +(const ComplexVariable & c, complex<double> num){
-        return ComplexVariable(c._a, c._b, c._c , c._im + num);
+    ComplexVariable operator+(const ComplexVariable &c, complex<double> num) {
+        return ComplexVariable(c._a, c._b, c._c, c._im + num);
     }
 
-    ComplexVariable operator -(const ComplexVariable & c1, const ComplexVariable & c2){
+    ComplexVariable operator-(const ComplexVariable &c1, const ComplexVariable &c2) {
         return ComplexVariable(c1._a - c2._a, c1._b - c2._b, c1._c - c2._c, c1._im - c2._im);
     }
 
-    ComplexVariable operator -(const ComplexVariable & c, const double num){
+    ComplexVariable operator-(const ComplexVariable &c, const double num) {
         return ComplexVariable(c._a, c._b, c._c - num, c._im);
     }
 
-    ComplexVariable operator*(const double num, ComplexVariable const & c) {
-        return c._a > 0 ? ComplexVariable(c._a * num, c._b, c._c, c._im) : ComplexVariable(c._a, c._b * num, c._c, c._im);
+    ComplexVariable operator*(const double num, ComplexVariable const &c) {
+        return c._a > 0 ? ComplexVariable(c._a * num, c._b, c._c, c._im) : ComplexVariable(c._a, c._b * num, c._c,
+                                                                                           c._im);
     }
-    ComplexVariable operator /(ComplexVariable const & c, const double num){
-        if(num == 0){
+
+    ComplexVariable operator/(ComplexVariable const &c, const double num) {
+        if (num == 0) {
             throw invalid_argument("Can't divide by zero");
         }
 
-        return c._a > 0 ? ComplexVariable(c._a/num, c._b, c._c, c._im) : ComplexVariable(c._a, c._b / num, c._c, c._im);
+        return c._a > 0 ? ComplexVariable(c._a / num, c._b, c._c, c._im) : ComplexVariable(c._a, c._b / num, c._c,
+                                                                                           c._im);
     }
 
-    ComplexVariable operator^(ComplexVariable const & c, const double num) {
-        if(num == 0){
+    ComplexVariable operator^(ComplexVariable const &c, const double num) {
+        if (num == 0) {
             throw invalid_argument("can't power by zero");
         }
         return ComplexVariable(1, 0, c._c, c._im);
     }
 
-    ComplexVariable operator ==(const ComplexVariable & c, const double num){
+    ComplexVariable operator==(const ComplexVariable &c, const double num) {
         return ComplexVariable(c._a, c._b, c._c - num, c._im);
     }
 
-    ComplexVariable operator ==(const ComplexVariable & c1, const ComplexVariable & c2){
+    ComplexVariable operator==(const ComplexVariable &c1, const ComplexVariable &c2) {
         return ComplexVariable(c1._a - c2._a, c1._b - c2._b, c1._c - c2._c, c1._im - c2._im);
     }
 
-    complex<double> ComplexVariable::solve_abc_formula(void)
-    {
+    complex<double> ComplexVariable::solve_abc_formula(void) {
         double a(_a), b(_b), c(_c);
-        double discriminant = b*b - 4*a*c;
+        double discriminant = b * b - 4 * a * c;
         complex<double> result(0.0, 0);
 
-        if(a == 0.0 && b == 0.0)
-        {
+        if (a == 0.0 && b == 0.0) {
             throw invalid_argument("No real soultion exists");
         }
-        if(a != 0)//It is possible to be a Linear equation
+        if (a != 0)//It is possible to be a Linear equation
         {
             if (discriminant >= 0) {
                 result = (-b + sqrt(discriminant)) / (2 * a);//One solution is enough
@@ -148,8 +147,8 @@ namespace solver{
                 result.real(-b / (2 * a));
                 result.imag(sqrt(-discriminant) / (2 * a));
             }
-        } else{
-            result.real(-c/b); //The only solution we have
+        } else {
+            result.real(-c / b); //The only solution we have
             result.imag(_im.imag());
         }
 
--- solver.hpp
+++ solver.hpp
@@ -2,29 +2,32 @@
 #define SOLVER_A_SOLVER_HPP
 
 #include <complex>
+
 using namespace std;
 
-namespace solver{
+namespace solver {
 
     //Forward declaration
     class RealVariable;
+
     class ComplexVariable;
 
     double solve(RealVariable);//Function has to clear the varables of the reference
     complex<double> solve(ComplexVariable);
 
-    class Variable{
+    class Variable {
     protected:
         double _a;//Coefficient of x^2
         double _b;//Coefficient of x
         double _c;//Free number
 
-        Variable(): _a(0.0), _b(1.0), _c(0.0){}//Default C'tor
-        Variable(double a, double b, double c): _a(a), _b(b), _c(c){}
+        Variable() : _a(0.0), _b(1.0), _c(0.0) {}//Default C'tor
+        Variable(double a, double b, double c) : _a(a), _b(b), _c(c) {}
+
         virtual complex<double> solve_abc_formula(void) = 0;//Yes i make it pure virtual => abstract class
     };
 
-    class RealVariable: public Variable{
+    class RealVariable : public Variable {
         // Inherit Base's constructors, default ctor is Inherited by default(if not other Ctor implemented)
         using Variable::Variable;
 
@@ -34,36 +37,58 @@ namespace solver{
         complex<double> solve_abc_formula(void) override;
 
         //We want make it friend in order to enable something like 7 * RealVariable
-        friend RealVariable operator +(const RealVariable &, const RealVariable &);
-        friend RealVariable operator +(const RealVariable &, const double);
-        friend RealVariable operator +(const double, const RealVariable &);
-        friend RealVariable operator -(const RealVariable &, const RealVariable &);
-        friend RealVariable operator -(const RealVariable &, const double);
-        friend RealVariable operator *(const double, const RealVariable &);
-        friend RealVariable operator /(const RealVariable &, const double);
-        friend RealVariable operator ^(RealVariable const &, const double);
-        friend RealVariable operator ==(const RealVariable &, const RealVariable &);
-        friend RealVariable operator ==(const RealVariable &, const double);
+        friend RealVariable operator+(const RealVariable &, const RealVariable &);
+
+        friend RealVariable operator+(const RealVariable &, const double);
+
+        friend RealVariable operator+(const double, const RealVariable &);
+
+        friend RealVariable operator-(const RealVariable &, const RealVariable &);
+
+        friend RealVariable operator-(const RealVariable &, const double);
+
+        friend RealVariable operator*(const double, const RealVariable &);
+
+        friend RealVariable operator/(const RealVariable &, const double);
+
+        friend RealVariable operator^(RealVariable const &, const double);
+
+        friend RealVariable operator==(const RealVariable &, const RealVariable &);
+
+        friend RealVariable operator==(const RealVariable &, const double);
     };
 
-    class ComplexVariable: public Variable {
+    class ComplexVariable : public Variable {
         complex<double> _im;
-        ComplexVariable(double a, double b, double c, complex<double> im): Variable(a,b,c), _im(im){}
+
+        ComplexVariable(double a, double b, double c, complex<double> im) : Variable(a, b, c), _im(im) {}
 
     public:
         complex<double> solve_abc_formula(void) override;
-        ComplexVariable(): Variable(), _im(0.0){};
-        friend ComplexVariable operator +(const ComplexVariable &, const ComplexVariable &);
-        friend ComplexVariable operator +(const ComplexVariable &, const double);
-        friend ComplexVariable operator +(const double, const ComplexVariable &);
-        friend ComplexVariable operator +(const ComplexVariable &, complex<double>);
-        friend ComplexVariable operator -(const ComplexVariable &, const ComplexVariable &);
-        friend ComplexVariable operator -(const ComplexVariable &, const double);
-        friend ComplexVariable operator *(const double, ComplexVariable const &);
-        friend ComplexVariable operator /(ComplexVariable const &, const double);
-        friend ComplexVariable operator ^(ComplexVariable const &, const double);
-        friend ComplexVariable operator ==(const ComplexVariable &, const double);
-        friend ComplexVariable operator ==(const ComplexVariable &, const ComplexVariable &);
+
+        ComplexVariable() : Variable(), _im(0.0) {};
+
+        friend ComplexVariable operator+(const ComplexVariable &, const ComplexVariable &);
+
+        friend ComplexVariable operator+(const ComplexVariable &, const double);
+
+        friend ComplexVariable operator+(const double, const ComplexVariable &);
+
+        friend ComplexVariable operator+(const ComplexVariable &, complex<double>);
+
+        friend ComplexVariable operator-(const ComplexVariable &, const ComplexVariable &);
+
+        friend ComplexVariable operator-(const ComplexVariable &, const double);
+
+        friend ComplexVariable operator*(const double, ComplexVariable const &);
+
+        friend ComplexVariable operator/(ComplexVariable const &, const double);
+
+        friend ComplexVariable operator^(ComplexVariable const &, const double);
+
+        friend ComplexVariable operator==(const ComplexVariable &, const double);
+
+        friend ComplexVariable operator==(const ComplexVariable &, const ComplexVariable &);
     };
 }
 
